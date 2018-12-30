a0524422c1cdab631eaafa459197422382b36590
contact.englishmate@gmail.com <contact.englishmate@gmail.com>
2018-12-30 09:22:56 +0000

    init

---
+++ py/github-Copy1.py
@@ -0,0 +1,11 @@
+# # Usage
+# 1. structure your code like this: 
+#   - `/data/...` insert your data here
+#   - `/ipynb/...` your workspace, ignore git diff in this folder
+#   - `/py/...` your directory for code diff, much easier to read here
+#   - `/github.ipynb` this file
+# 
+# # Initialize
+# check if git is installed
+get_ipython().system(u' git --version')
+print('hello')
